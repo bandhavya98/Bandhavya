3f58f55732f919d27b0e0c26ac4eefe993cb9554
bandhavya98 <63386663+bandhavya98@users.noreply.github.com>
2022-08-20 18:45:49 +0530

    Update nestedloop.ps1

--- Challenges/challenge3/nestedloop.ps1
+++ Challenges/challenge3/nestedloop.ps1
@@ -1,1 +1,18 @@
 
+$var1=@(Read-Host "Enter the nested object")
+
+$var2=Read-Host "Type key"
+
+$var3= $var1 -split'}|{|:'
+$key= $var2 -split'/'
+
+$var4=@($var3[1,3,5,6])
+
+for ( $i=0; $i -le ($var4.Length -1); $i +=1 )
+{
+for ( $j=0; $j -le ($key.Length -1); $j +=1 )
+{
+if ($var4[$i] -ne $key[$j])
+{echo $var4[$i]}
+}
+}
