998d452bc4884c937e2f5c782457c4c39012fd06
bandhavya98 <63386663+bandhavya98@users.noreply.github.com>
2022-08-20 23:25:54 +0530

    Update nestedloop.ps1

--- Challenges/challenge3/nestedloop.ps1
+++ Challenges/challenge3/nestedloop.ps1
@@ -1,18 +1,17 @@
+$var1=Read-Host "Enter the nested object"
+$var2=Read-Host "Enter the key in the foramt a/b/c"
 
-$var1=@(Read-Host "Enter the nested object")
-
-$var2=Read-Host "Type key"
 
 $var3= $var1 -split'}|{|:'
-$key= $var2 -split'/'
+$key= @($var2 -split'/')
 
-$var4=@($var3[1,3,5,6])
+$narr=@($var3[1,3,5,6])
 
-for ( $i=0; $i -le ($var4.Length -1); $i +=1 )
+for ( $i=0; $i -le ($narr.Length -1); $i +=1 )
 {
-for ( $j=0; $j -le ($key.Length -1); $j +=1 )
-{
-if ($var4[$i] -ne $key[$j])
-{echo $var4[$i]}
-}
+$trimnarr=$narr[$i].Substring($narr[$i].length -2, 1)
+$splitnarr=@($trimnarr -split '')
+$finalarr=@($splitnarr[1,4,7,10])
+$value=$finalarr | where {$key -notcontains $_}
+echo $value
 }
