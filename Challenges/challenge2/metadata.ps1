2d85b2da13b392c58e22bca2d037f78d48631306
bandhavya98 <63386663+bandhavya98@users.noreply.github.com>
2022-08-20 18:43:01 +0530

    Add files via upload

--- Challenges/challenge2/metadata.ps1
+++ Challenges/challenge2/metadata.ps1
@@ -1,17 +1,17 @@
-Select-AzSubscription -SubscriptionId ''
+Select-AzSubscription -SubscriptionId 'f2851833-08ee-4fb5-b9ab-5146fd57e53c'
 $VMs = Get-AzVM
 $data = @()
 foreach($VirtualMachine in $VMs){
 $vm=Get-AzVm -Name $VirtualMachine.Name
-$VmSize=((Get-AzVM -ResourceGroupName $VirtualMachine.ResourceGroupName -Name $VirtualMachine.Name).HardwareProfile
+$VmSize=((Get-AzVM -ResourceGroupName $VirtualMachine.ResourceGroupName -Name $VirtualMachine.Name).HardwareProfile).VmSize
 $nic = $vm.NetworkProfile.NetworkInterfaces
 $networkinterface = ($nic.id -split '/')[-1]
 $NetworkDetails = Get-AzNetworkInterface -Name $networkinterface
 $Vnet=($NetworkDetails.IpConfigurations.Subnet.Id -split '/')[-3] 
 $Subnet=($NetworkDetails.IpConfigurations.Subnet.Id -split '/')[-1] 
 $IP=$NetworkDetails.IpConfigurations.PrivateIpAddress 
-$OSDiskSize=((Get-AzVM -ResourceGroupName $VirtualMachine.ResourceGroupName -Name $VirtualMachine.Name).StorageProfile).OSDisk
-$DataDiskSize=((Get-AzVM -ResourceGroupName $VirtualMachine.ResourceGroupName -Name $VirtualMachine.Name).StorageProfile).DataDisk
+$OSDiskSize=(((Get-AzVM -ResourceGroupName $VirtualMachine.ResourceGroupName -Name $VirtualMachine.Name).StorageProfile).OSDisk).DiskSizeGB
+$DataDiskSize=(((Get-AzVM -ResourceGroupName $VirtualMachine.ResourceGroupName -Name $VirtualMachine.Name).StorageProfile).DataDisk).DiskSizeGB
 $row = New-Object PSObject
 $row | Add-Member -MemberType NoteProperty -Name "SL_no" -Value $data.Count
 $row | Add-Member -MemberType NoteProperty -Name "ResourceGroup Name" -Value $VirtualMachine.ResourceGroupName
@@ -23,8 +23,8 @@ $row | Add-Member -MemberType NoteProperty -Name "Location" -Value $VirtualMachi
 $row | Add-Member -MemberType NoteProperty -Name "Virtual Network" -Value $Vnet
 $row | Add-Member -MemberType NoteProperty -Name "Subnet" -Value $Subnet
 $row | Add-Member -MemberType NoteProperty -Name "IP" -Value $IP
-$row | Add-Member -MemberType NoteProperty -Name "OSDiskSize" -Value OSDiskSize.DiskSizeGB
-$row | Add-Member -MemberType NoteProperty -Name "DataDiskSize" -Value DataDiskSize.DiskSizeGB
+$row | Add-Member -MemberType NoteProperty -Name "OSDiskSize" -Value $OSDiskSize
+$row | Add-Member -MemberType NoteProperty -Name "DataDiskSize" -Value $DataDiskSize
 $data += $row
 $data | Export-Csv "VMDetails.csv" -NoTypeInformation -Append
 }
